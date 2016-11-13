187965e15213aa27e32c89592dd6c16f086c15b1
grogcw <grogcw@gmail.com>
2016-11-13 06:50:41 +0100

    Create DEBIAN_mountscript.sh

---
+++ DEBIAN_mountscript.sh
@@ -0,0 +1,79 @@
+#/bin/bash
+
+#DEFIINES MOUNTFOLDER & IP_POOL
+MOUNTFOLDER=/mnt
+
+IP_POOL="192.168.0."
+echo "IP Pool is "$IP_POOL"XXX"
+
+#ASKS IP TERMINATION
+read -p "IP Address (enter or q to quit) : " IP_ADDRESS_TERM
+
+if [ -z "$IP_ADDRESS_TERM" ] || [[ "$IP_ADDRESS_TERM" = "q" ]] || [[ "$IP_ADDRESS_TERM" = "Q" ]];
+	then
+exit
+	else
+
+#CHECK IF IP IS CORRECT
+stat=0
+
+IP_ADDRESS=$IP_POOL$IP_ADDRESS_TERM
+
+if [[ $IP_ADDRESS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=1
+ fi
+
+if [[ $stat = 1 ]]; then
+
+#TRY TO CONNECT
+smbclient -L $IP_ADDRESS -N
+
+#CHECK COMMAND SUCCESS
+retval=$?
+if [ $retval -ne 0 ]; then
+    exit
+fi
+
+#ASK FOR SHARENAME
+read -p "Sharename (enter or q to quit) : " SHARENAME
+if [ -z "$SHARENAME" ] || [[ "$SHARENAME" = "q" ]] || [[ "$SHARENAME" = "Q" ]];
+        then
+exit
+fi
+
+#ASK FOR USERNAME - GUEST IS DEFAULT
+read -p "Username [GUEST] : " USERNAME
+USERNAME=${USERNAME:-GUEST}
+
+#ASK FOR PASSWORD - GUEST IS DEFAULT
+read -p "Password [GUEST] : " PASSWORD
+PASSWORD=${PASSWORD:-GUEST}
+
+#CREATES MOUNTPOINT
+mkdir /mnt/"$SHARENAME"
+
+#MOUNTS
+mount -t cifs //$IP_ADDRESS/"$SHARENAME" -o username=$USERNAME,password=$PASSWORD $MOUNTFOLDER/"$SHARENAME"
+
+#CHECKS IF SUCCESS
+retval=$?
+if [ $retval -ne 0 ]; then
+	exit
+fi
+
+#SAVES MOUNTPOINT TO log_mountscript.log
+echo \"$MOUNTFOLDER/"$SHARENAME"\" > log_mountscript.log
+
+echo "Mounting of "$MOUNTFOLDER"/"$SHARENAME" succesful !"
+
+else
+echo "Invalid ip"
+exit
+fi
+fi
