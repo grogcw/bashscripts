5c17b540d3f2a11b0ed28885187f46c57905b0ae
grogcw <grogcw@gmail.com>
2017-09-14 03:38:39 +0200

    Update RPI_checkvoltage.sh

--- RPI_checkvoltage.sh
+++ RPI_checkvoltage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo -e "" ; \
-echo -e "############### VOLTAGE ################" ; \
+#echo -e "\033[1;33m########## VOLTAGES ##########\033[0m" ; \
+#echo -e "" ; \
+echo -e " \033[1;31m--- CPU Voltage ---\033[0m" ; \
+echo -e " core : $(vcgencmd measure_volts core)" ; \
 echo -e "" ; \
-echo -e "--- CPU Voltage ---" ; \
-echo -e "core : $(vcgencmd measure_volts core)" ; \
-echo -e "" ; \
-echo -e "---- RAMS Voltage ----" ; \
+echo -e "\033[1;32m---- RAMS Voltage ----\033[0m" ; \
 for id in sdram_c sdram_i sdram_p ; do \
         echo -e "$id : $(vcgencmd measure_volts $id)" ; \
 done
-echo -e "" ; \
-echo -e "########################################" ; \
+#echo -e "" ; \
+#echo -e "\033[1;33m##############################\033[0m" ; \
 echo -e "" ; \
