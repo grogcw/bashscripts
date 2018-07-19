958de6994ac5a9bd02e8994b5a16ece87216d41f
grogcw <grogcw@gmail.com>
2018-07-19 23:27:37 +0200

    Update RPI_checkhealth.sh

--- RPI_checkhealth.sh
+++ RPI_checkhealth.sh
@@ -12,6 +12,9 @@ echo -e "\033[1;32m---- RAMS Voltage ----\033[0m" ; \
 for id in sdram_c sdram_i sdram_p ; do \
         echo -e "$id : $(vcgencmd measure_volts $id)" ; \
 done
+echo -e "" ; \
+echo -e " \033[1;34m--- RAM Split ---\033[0m" ; \
+echo -e "$(vcgencmd get_mem arm && vcgencmd get_mem gpu)" ; \
 #echo -e "" ; \
 #echo -e "\033[1;33m##############################\033[0m" ; \
 echo -e "" ; \
