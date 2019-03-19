8ae92a3b0f3253acd013042e820a412e9d2991cd
grogcw <grogcw@gmail.com>
2019-03-19 01:21:29 +0100

    Update CLI_script_aria2c.sh

--- CLI_script_aria2c.sh
+++ CLI_script_aria2c.sh
@@ -21,16 +21,24 @@ echo ""
 unset options i
 while IFS= read -r -d $'\0' f; do
 	options[i++]="$f"
-done < <(find ./ -maxdepth 1 -type f -name "*.txt" -print0 )
-if [ -z "${options[0]}" ]
-then
-	echo -e "${RED}No valid .txt list found in current directory,${NC} exiting."
-	exit
-fi
-select opt in "${options[@]}" "Quit"; do
+done < <( find ./ -maxdepth 1 -type f -name "*.txt" -print0 )
+#if [ -z "${options[0]}" ]
+#then
+#	echo -e "${RED}No valid .txt list found in current directory,${NC} exiting."
+#	exit
+#fi
+select opt in "${options[@]}" "Paste" "Quit"; do
 	case $opt in
 		*.txt)
 			echo "$opt selected"
+			break
+			;;
+		"Paste")
+printf "\n"
+read -d "@" -p "Paste links here (then press \"@\" when done) :
+" paste
+			echo "$paste" > ./Paste
+printf "\n"
 			break
 			;;
 		"Quit")
@@ -42,6 +50,25 @@ select opt in "${options[@]}" "Quit"; do
 			;;
 	esac
 done
+
+#CHECK IF FILE IS EMPTY
+if [ -f $opt ]
+then
+	if ! [ -s $opt ]
+	then
+		echo "File exists but empty."
+		exit
+	fi
+	count=$(wc -c < $opt)
+	if [ $count -lt 4 ]
+	then
+		echo "File is empty."
+		exit
+	fi
+else
+	echo "File not exists."
+	exit
+fi
 
 #ASKS DOWNLOAD DESTINATION
 read -e -p "Download Folder (enter or q to quit) : " targetdir
